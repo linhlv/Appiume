232e5ddd8808216421fe42f833aa08a3f5a70de8
devlinhle <dev.linhlv@gmail.com>
2016-09-10 22:05:51 +0700

    Add more event handler

---
+++ Appiume.Web/Dewey/Core/Events/Notifications/EventCancelFirebaseNotification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Appiume.Apm.Dependency;
+using Appiume.Apm.Events.Bus.Handlers;
+
+namespace Appiume.Web.Dewey.Core.Events.Notifications
+{
+    public class EventCancelFirebaseNotification : IEventHandler<EventCancelledEvent>, ITransientDependency
+    {
+        public void HandleEvent(EventCancelledEvent eventData)
+        {
+           
+        }
+    }
+}
